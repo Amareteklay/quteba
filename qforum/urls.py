a5b7e4c909cd6a72aa091e47eb8c3f8112cbe94a
Amare Teklay Hailu <amareteklay@gmail.com>
2022-04-17 13:08:48 +0000

    templates for create and detail forums added

--- qforum/urls.py
+++ qforum/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.ThreadList.as_view(), name='threads'),
     path('category/', views.CategoryList.as_view(), name='categories'),
+    path('create/', views.CreateForum.as_view(), name='create_forum'),
 ]
--- qforum/views.py
+++ qforum/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views import generic
+from django.views.generic.edit import CreateView
 from .models import Thread, Post, Category
+from .forms import ThreadForm
 
 
 class ThreadList(generic.ListView):
@@ -16,3 +18,23 @@ class PostList(generic.ListView):
 class CategoryList(generic.ListView):
     model = Category
     template_name = 'qforum/forum_base.html'
+
+
+
+class CreateForum(CreateView):
+    model = Thread
+    fields = ['topic', 'description']
+    template_name = 'qforum/create_forum.html'
+
+    def post(request):
+        pass
+
+
+class VoteUpView(generic.View):
+    def post(request):
+        pass
+
+
+class VoteDownView(generic.View):
+    def post(request):
+        pass
