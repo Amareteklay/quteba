8f446d24ac2da85a0043cf027f64b2dc9340741b
Amare Teklay Hailu <amareteklay@gmail.com>
2022-04-22 19:27:51 +0000

    deleted unused code in forum view

--- qforum/views.py
+++ qforum/views.py
@@ -25,45 +25,6 @@ class CategoryList(generic.ListView):
     template_name = 'qforum/forum_base.html'
 
 
-class AThreadDetailView(DetailView):
-    model = Thread
-    context_object_name = 'thread'
-    template_name = 'qforum/thread_detail.html'
-
-
-def thread_detail(request, thread):
-    thread = get_object_or_404(Thread, slug=thread, status=1)
-    comments = thread.comments.filter(active=True)
-    new_comment = None
-    comment_form = CommentForm()
-    if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            new_comment = comment_form.save(commit=False)
-            new_comment.thread = thread
-            new_comment.save()
-            print('Here')
-            return redirect(thread.get_absolute_url()+'#'+str(new_comment.id))
-        else:
-            comment_form = CommentForm()
-    return render(request, 'qforum/thread_detail.html', {"thread": thread, "comments": comments, "comment_form": comment_form})
-
-
-def reply_view(request):
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            thread_id = request.POST.get('thread_id')
-            parent_id = request.POST.get('parent')
-            thread_url = request.POST.get('thread_url')
-            reply = form.save(commit=False)
-            reply.thread = Thread(id=thread_id)
-            reply.parent = Comment(id=parent_id)
-            reply.save()
-            return redirect(thread_url+'#'+str(reply.id))
-    return redirect("/") 
-
-
 class ThreadDetailView(DetailView):
     model = Thread
     context_object_name = 'thread'
@@ -84,6 +45,7 @@ class ThreadDetailView(DetailView):
         if form.is_valid():
             thread_id = request.POST.get('thread_id')
             parent_id = request.POST.get('parent')
+            print(parent_id)
             thread_url = request.POST.get('thread_url')
             reply = form.save(commit=False)
             reply.thread = Thread(id=thread_id)
@@ -98,17 +60,6 @@ class CreateForum(LoginRequiredMixin, CreateView):
     model = Thread
     form_class = ThreadForm
     template_name = 'qforum/create_forum.html'
-    success_url = reverse_lazy('threads')
-
-    def form_valid(self, form):
-        form.instance.name = self.request.user
-        return super().form_valid(form)
-
-
-class CreateComment(LoginRequiredMixin, CreateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'qforum/comment.html'
     success_url = reverse_lazy('threads')
 
     def form_valid(self, form):
