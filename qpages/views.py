6d4c50f46e532f145d3981d3d7731290d53e1e87
Amare Teklay Hailu <amareteklay@gmail.com>
2022-05-03 08:52:39 +0000

    added sections to home page

--- qpages/views.py
+++ qpages/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout 
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from qblog.models import Post
+from qforum.models import Thread
 
 
 def index(request):
-    return render(request, 'index.html')
+    recent_posts = Post.objects.filter(status=1).order_by('-created_on')
+    active_topics = Thread.objects.filter(status=1).order_by('-created_on')
+    return render(request, 'index.html', {'post': recent_posts, 'thread': active_topics})
 
 def about(request):
     return render(request, 'qpages/about.html')
--- users/views.py
+++ users/views.py
@@ -9,8 +9,8 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST) 
         if form.is_valid():
-            form.save() 
             username = form.cleaned_data.get('username') 
+            form.save() 
             messages.success(request, f'Your account has been created! You are now able to log in') 
             return redirect('login')
     else:
