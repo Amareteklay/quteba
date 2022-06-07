05bfd799c7dd3166140f13a6833666dfd1d47271
Amare Teklay Hailu <amareteklay@gmail.com>
2022-06-07 09:52:00 +0000

    Format message display and change templates for login and logout

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('signup/', views.register, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
     path('<username>/update/', views.update_profile, name='update-profile'),
     path('<pk>/delete/', views.delete_profile, name='delete-profile')
 ]
