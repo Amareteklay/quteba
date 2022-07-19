a8a07deb57bdf382f07d49fd1e146fba49a63b31
Amare Teklay Hailu <amareteklay@gmail.com>
2022-07-19 12:27:15 +0000

    Remove unused import and Make final migrations

--- qforum/views.py
+++ qforum/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
+from django.views import View
 from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
